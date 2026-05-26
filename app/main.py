6e1532c045ad1870140a29343aabb6651d789417
Anderson <anderson.medeiros3011@gmail.com>
2026-05-26 19:50:27 -0300

    style: adiciona tipagem e corrige formatacao do flake8

--- app/main.py
+++ app/main.py
@@ -4,16 +4,24 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-
 def cinema_visit(
     customers: list,
     hall_number: int,
     cleaner: str,
     movie: str
-):
+) -> None:
     customer_objects = [Customer(c["name"], c["food"]) for c in customers]
     cleaner_obj = Cleaner(cleaner)
     hall = CinemaHall(hall_number)
+
     for customer in customer_objects:
-        CinemaBar.sell_product(product=customer.food, customer=customer)
-    hall.movie_session(movie_name=movie, customers=customer_objects, cleaning_staff=cleaner_obj)
+        CinemaBar.sell_product(
+            product=customer.food,
+            customer=customer
+        )
+
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_objects,
+        cleaning_staff=cleaner_obj
+    )
