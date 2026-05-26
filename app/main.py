c918d9e53005439dacbde935a11fe9a7f9bf4c58
Anderson <anderson.medeiros3011@gmail.com>
2026-05-26 19:44:37 -0300

    feat: cria estrutura do cinema e implementa a funcao cinema_visit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+):
+    customer_objects = [Customer(c["name"], c["food"]) for c in customers]
+    cleaner_obj = Cleaner(cleaner)
+    hall = CinemaHall(hall_number)
+    for customer in customer_objects:
+        CinemaBar.sell_product(product=customer.food, customer=customer)
+    hall.movie_session(movie_name=movie, customers=customer_objects, cleaning_staff=cleaner_obj)
